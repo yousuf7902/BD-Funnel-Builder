5232efdf501bfd0af369d77ebefd8757b3b5f86e
yousuf7902 <yousufhassan04@gmail.com>
2025-01-08 10:21:20 +0600

    2 sections are done

--- pages/index.vue
+++ pages/index.vue
@@ -34,17 +34,52 @@
                         এক নজরে</h1>
 
                     <div class="grid grid-cols-3 gap-[50px] justify-center items-center">
-                        <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px]">
-                            <h3 class="card__heading">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
-                            <p class="card__contents">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px] first:bg-[#1B4B36]">
+                            <h3 class="text-[36px] font-[600] leading-[48px] first:text-[#EBFF3D]">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
+                            <p class="text-[#ffffff] text-[24px] leading-[36px] mt-[5px]">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
                                 রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
                                 স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
                         </div>
 
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px]">
+                            <h3 class="text-[36px] font-[600] leading-[48px] text-[#1B4B36]">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
+                            <p class="text-[#000000] text-[24px] leading-[36px] mt-[5px]">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+                        </div>
+
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px]">
+                            <h3 class="text-[36px] font-[600] leading-[48px] text-[#1B4B36]">ফিটনেস ট্র্যাকিং</h3>
+                            <p class="text-[#000000] text-[24px] leading-[36px] mt-[5px]">স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড।
+                            ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন।
+                            স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+                        </div>
+
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px]">
+                            <h3 class="text-[36px] font-[600] leading-[48px] text-[#1B4B36]">স্মার্ট নোটিফিকেশন</h3>
+                            <p class="text-[#000000] text-[24px] leading-[36px] mt-[5px]">কল, মেসেজ, এবং অ্যাপ নোটিফিকেশন সরাসরি আপনার হাতে।
+                            গুরুত্বপূর্ণ আপডেট কখনোই মিস করবেন না।</p>
+                        </div>
+
+                        <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px]">
+                            <h3 class="text-[36px] font-[600] leading-[48px] text-[#1B4B36]">দীর্ঘস্থায়ী ব্যাটারি লাইফ</h3>
+                            <p class="text-[#000000] text-[24px] leading-[36px] mt-[5px]">একবার চার্জে ৭+ দিন ব্যবহারের সুবিধা।
+                            দ্রুত চার্জিং প্রযুক্তি।</p>
+                        </div>
+
+                         <div class="max-w-[406px] min-h-[397px] border-[3px] border-[#1B4B36] py-[25px] px-[15px] text-center rounded-tl-[35px] rounded-br-[35px]">
+                            <h3 class="text-[36px] font-[600] leading-[48px] text-[#1B4B36]">মিউজিক এবং ক্যামেরা
+                            নিয়ন্ত্রণ</h3>
+                            <p class="text-[#000000] text-[24px] leading-[36px] mt-[5px]">মিউজিক চালানো বা ক্যামেরা ক্লিক করুন আপনার স্মার্টওয়াচ থেকে।
+                            সহজ এবং সাবলীল ব্যবহারের সুবিধা।</p>
+                        </div>
+
                     </div>
-                    <div class="product-feature__button">
-                        <button class="product-feature__btn btn">অর্ডার করতে ক্লিক করুন <i
-                                class="fa-solid fa-hand-pointer hand-icon"></i></button>
+                    <div
+                        class="flex justify-center items-center my-[50px] text-[#EBFF3D] text-[48px] font-[700] leading-[65px] ">
+                        <button class="py-[16px] px-[30px] bg-[#1B4B36] rounded-[48px]">অর্ডার করতে
+                            ক্লিক
+                            করুন 👉</button>
                     </div>
                 </div>
             </section>
